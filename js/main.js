856ba1a465223152c083dd8182b2fb5abd634cbc
Ismoil2009 <osnovavbs092009@gmail.com>
2023-05-20 13:38:43 +0500

    1 page left

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(".owl-dish").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+
+$(".owl-cust").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 4.7,
+    },
+  },
+});
